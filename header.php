738fb055eba2ac0e58615717cba3772beb1d22be
OmarManganese <78893144+OmarManganese@users.noreply.github.com>
2021-09-04 09:35:10 +0200

    Added Bootstrap with CDN and the Bootstrap navbar in the header

--- header.php
+++ header.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
-<html lang="it">
+<html lang="eng">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
     <title>WPizza</title>
   </head>
 
   <body>
     <header>
-      <h1>Header</h1>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">             
+                <li class="nav-item">
+                  <a class="nav-link" href="#menu">Menù</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#contact">Contact</a>
+                </li>               
+                <li class="nav-item">
+                  <a class="nav-link" href="#where-we-are">Where We Are</a>
+                </li>               
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
     </header>
